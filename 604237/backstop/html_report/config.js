92d668bd50b98b0ea3277bc0015bb34ec5d81888
Ian Bromwich <ian.bromwich90@gmail.com>
2023-06-14 20:37:04 +0000

    Build 604237

---
+++ 604237/backstop/html_report/config.js
@@ -0,0 +1,299 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Homepage_Test_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Homepage_Test_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Homepage_Test_0_document_0_desktop.png",
+        "label": "Homepage Test",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/",
+        "referenceUrl": "http://sheffield.ac.uk/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.05",
+          "analysisTime": 217
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Accordion_click_test_desktop__accessibility_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Accordion_click_test_desktop__accessibility_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Accordion_click_test_desktop__accessibility_0_document_0_desktop.png",
+        "label": "Accordion click test desktop /accessibility",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/accessibility",
+        "referenceUrl": "http://sheffield.ac.uk/accessibility",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.01",
+          "analysisTime": 418
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Accordion_click_test_desktop__accessibility_0_document_0_phone.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Accordion_click_test_desktop__accessibility_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Accordion_click_test_desktop__accessibility_0_document_0_phone.png",
+        "label": "Accordion click test desktop /accessibility",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/accessibility",
+        "referenceUrl": "http://sheffield.ac.uk/accessibility",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.01",
+          "analysisTime": 190
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default__management_home_page_-_LOGOS_0_document_0_phone.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default__management_home_page_-_LOGOS_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default__management_home_page_-_LOGOS_0_document_0_phone.png",
+        "label": "/management home page - LOGOS!",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/management",
+        "referenceUrl": "http://sheffield.ac.uk/management",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default__management_home_page_-_LOGOS_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default__management_home_page_-_LOGOS_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default__management_home_page_-_LOGOS_0_document_1_tablet.png",
+        "label": "/management home page - LOGOS!",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/management",
+        "referenceUrl": "http://sheffield.ac.uk/management",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default__management_home_page_-_LOGOS_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default__management_home_page_-_LOGOS_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default__management_home_page_-_LOGOS_0_document_2_desktop.png",
+        "label": "/management home page - LOGOS!",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/management",
+        "referenceUrl": "http://sheffield.ac.uk/management",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_PG_course_page_1_0_document_0_phone.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Test_PG_course_page_1_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Test_PG_course_page_1_0_document_0_phone.png",
+        "label": "Test PG course page #1",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/postgraduate/taught/courses/2022/test-pgt-course-2022-bmedsci-bmus-llm",
+        "referenceUrl": "http://sheffield.ac.uk/postgraduate/taught/courses/2022/test-pgt-course-2022-bmedsci-bmus-llm",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.13",
+          "analysisTime": 145
+        },
+        "diffImage": "../bitmaps_test/20230614-203527/failed_diff_backstop_default_Test_PG_course_page_1_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_PG_course_page_1_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Test_PG_course_page_1_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Test_PG_course_page_1_0_document_1_tablet.png",
+        "label": "Test PG course page #1",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/postgraduate/taught/courses/2022/test-pgt-course-2022-bmedsci-bmus-llm",
+        "referenceUrl": "http://sheffield.ac.uk/postgraduate/taught/courses/2022/test-pgt-course-2022-bmedsci-bmus-llm",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_PG_course_page_1_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Test_PG_course_page_1_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Test_PG_course_page_1_0_document_2_desktop.png",
+        "label": "Test PG course page #1",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/postgraduate/taught/courses/2022/test-pgt-course-2022-bmedsci-bmus-llm",
+        "referenceUrl": "http://sheffield.ac.uk/postgraduate/taught/courses/2022/test-pgt-course-2022-bmedsci-bmus-llm",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_PG_course_page_2_0_document_0_phone.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Test_PG_course_page_2_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Test_PG_course_page_2_0_document_0_phone.png",
+        "label": "Test PG course page #2",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/test/2021/test-pgt-course-2021-bsc-ma",
+        "referenceUrl": "http://sheffield.ac.uk/test/2021/test-pgt-course-2021-bsc-ma",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_PG_course_page_2_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Test_PG_course_page_2_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Test_PG_course_page_2_0_document_1_tablet.png",
+        "label": "Test PG course page #2",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/test/2021/test-pgt-course-2021-bsc-ma",
+        "referenceUrl": "http://sheffield.ac.uk/test/2021/test-pgt-course-2021-bsc-ma",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_PG_course_page_2_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20230614-203527/backstop_default_Test_PG_course_page_2_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Test_PG_course_page_2_0_document_2_desktop.png",
+        "label": "Test PG course page #2",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://sheffield.ac.uk/test/2021/test-pgt-course-2021-bsc-ma",
+        "referenceUrl": "http://sheffield.ac.uk/test/2021/test-pgt-course-2021-bsc-ma",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
